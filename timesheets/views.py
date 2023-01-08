413e31e262e7c78cb12913a27a13b7fd08df39bc
Robert Gray <rgray@ttsa.net>
2023-01-08 15:04:13 -0800

    Got the notes page of the timesheet reports wrapping properly and creating new page if necessary.

--- timesheets/views.py
+++ timesheets/views.py
@@ -6,7 +6,9 @@ from employee.models import TimesheetUser
 from .utils import TimesheetUtil
 from django.utils import timezone
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter, landscape
+from reportlab.lib.pagesizes import letter, portrait
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph
 from django.http import FileResponse
 import datetime
 import io
@@ -60,35 +62,44 @@ def report(request):
     user = TimesheetUser.objects.get(user=User.objects.get(id=request.user.id))
     period = TimesheetPeriod.objects.get(id=request.GET.get("period_id"))
     entries = TimesheetEntry.objects.filter(user=user, period=period)
-    aggregate_data = {}
+    aggregate_data = {'total_hours': 0.00, 'total_pay': 0.00}
 
     buffer = io.BytesIO()
     x1 = 20
-    y1 = 575
+    y1 = 750
     deltay = 15
     p = canvas.Canvas(buffer)
-    p.setPageSize(landscape(letter))
+    p.setPageSize(portrait(letter))
     p.setFont("Helvetica", 15, leading=None)
-    p.drawString(x1, y1, "Timesheet Report: " + user.user.first_name + " " + user.user.last_name)
-    p.drawString(x1, y1-15, "Pay Period: " + str(period))
-    p.drawString(x1, y1-40, "Timesheet Entries")
+    p.drawString(x1, y1, "Timesheet Report for " + user.organization.name)
+    p.drawString(x1, y1-15, "Employee: " + user.user.first_name + " " + user.user.last_name)
+    p.drawString(x1, y1-40, "Pay Period: " + str(period))
+    p.drawString(x1, y1-60, "Timesheet Entries")
     p.setFont("Courier", 12, leading=None)
     x1 += 5
-    y1 -= 60
+    y1 -= 80
     column_headers = [{'width': 125, 'text': 'Time In'},
                       {'width': 125, 'text': 'Time Out'},
+                      {'width': 80, 'text': 'Rate/Hr'},
                       {'width': 90, 'text': 'Duration'},
                       {'width': 60, 'text': 'Pay'},
-                      {'width': 100, 'text': 'Notes'},
+
                       ]
     for ch in column_headers:
         p.drawString(x1, y1, ch['text'])
         x1 += ch['width']
+    x1 = 20
+    y1 -= 5
+    p.line(x1, y1, x1+500, y1)
     y1 -= deltay
     for e in entries:
+        pay = float(e.user.hourly_rate) * float(e.duration)
+        aggregate_data['total_hours'] += float(e.duration)
+        aggregate_data['total_pay'] += pay
         x1 = 25
         earray = [datetime.date.strftime(e.date_time_in, "%m-%d-%Y %H:%M"),
-                  datetime.date.strftime(e.date_time_out, "%m-%d-%Y %H:%M"), str(e.duration), "100.0", str(e.notes)]
+                  datetime.date.strftime(e.date_time_out, "%m-%d-%Y %H:%M"), '{:.2f}'.format(e.user.hourly_rate),
+                  str(e.duration), '{:.2f}'.format(pay)]
         i = 0
         p.setFont("Courier", 10, leading=None)
         for ch in column_headers:
@@ -96,7 +107,43 @@ def report(request):
             x1 += ch['width']
             i += 1
         y1 -= deltay
+    x1 = 20
+    p.line(x1, y1, x1+500, y1)
+    y1 += 3
+    p.line(x1, y1, x1+500, y1)
+    x1 = 25
+    x1 += column_headers[0]['width'] + column_headers[1]['width']
+    y1 -= deltay
+    p.drawString(x1, y1, "Total")
+    x1 += column_headers[2]['width']
+    p.drawString(x1, y1, '{:.2f}'.format(aggregate_data['total_hours']))
+    x1 += column_headers[3]['width']
+    p.drawString(x1, y1, '{:.2f}'.format(aggregate_data['total_pay']))
     p.showPage()
+    if(True):
+        x1 = 25
+        y1 = 750
+        p.setFont("Helvetica", 15, leading=None)
+        p.drawString(x1, y1, "Timesheet Entry Notes")
+        y1 -= 60
+        styleSheet = getSampleStyleSheet()
+        style = styleSheet['BodyText']
+        aW = 500    # available width and height
+        aH = 725
+        for e in entries:
+            in_time = datetime.date.strftime(e.date_time_in, "%m-%d-%Y %H:%M")
+            out_time = datetime.date.strftime(e.date_time_out, "%m-%d-%Y %H:%M")
+            par = Paragraph(in_time + " - " + out_time + ":  " + str(e.notes), style)
+            w, h = par.wrap(aW, aH)    # find required space
+            print("Width: ", str(w), '  Height: ', str(h))
+            if w <= aW and 2 * h <= aH:
+                aH = aH - h - 20       # reduce the available height
+                par.drawOn(p, x1, aH)
+                print("Static Width: ", str(aW), '  Height: ', str(aH))
+            else:
+                aH = 725
+                p.showPage()
+                par.drawOn(p, x1, aH)
     p.save()
     buffer.seek(0)
 
