5a3f86337a36733428eec1a74088ce363d8332bf
Robert Gray <rgray@ttsa.net>
2023-03-04 10:24:46 -0500

    Testing from production and local.  Production set to mail.teknvovention.com

--- mailer/views.py
+++ mailer/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .serializers import EmailSerializer
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+import re
 import json
 
 
 def email(request):
     if request.method == 'POST':
         data = json.loads(request.POST.get('email'))
-        # data = json.loads(request.POST.get('email'))
         serializer = EmailSerializer(data=data)
         if serializer.is_valid():
             # Send email
-            print(data['sender'])
-            send_mail(data['subject'], data['message'], 'john@john.com', data['recipients'], fail_silently='False')
+            recipients = re.split(r'[,;]', data['recipients'][0].replace(' ', ''))
+            for r in recipients:
+                email = EmailMessage(data['subject'], data['message'], to=[r],
+                                     reply_to=['mailer@mail.teknovention.com'])
+                # email = EmailMessage(data['subject'], data['message'], to=recipients)
+                email.send()
             serializer.save()
-            return JsonResponse({"message": "Email send and saved"})
+            return JsonResponse({"message": "Email sent and saved"})
         else:
             return JsonResponse(status=404, data={'status': 'false', 'message': 'Email failed'})
     return render(request, 'tsmanagement/managedashboard.html')
