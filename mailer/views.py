61a25b51cabba5627b14baf62e3ea4c75d2a4666
Robert Gray <rgray@ttsa.net>
2023-03-27 19:04:54 -0700

    Fixed multiple recipients being sent multiple times

--- mailer/views.py
+++ mailer/views.py
@@ -18,11 +18,11 @@ def email(request):
             # Send email
             recipients = re.split(r'[,;]', data['recipients'][0].replace(' ', ''))
             reply_tos = re.split(r'[,;]', data['reply_tos'][0].replace(' ', ''))
-            for r in recipients:
-                email = EmailMessage(data['subject'], data['message'], data['sender'], to=recipients,
-                                     reply_to=reply_tos)
-                # email = EmailMessage(data['subject'], data['message'], to=recipients)
-                email.send()
+            # for r in recipients:
+            email = EmailMessage(data['subject'], data['message'], data['sender'], to=recipients,
+                                 reply_to=reply_tos)
+            # email = EmailMessage(data['subject'], data['message'], to=recipients)
+            email.send()
             serializer.save()
             return JsonResponse({"message": "Email sent and saved"})
         else:
