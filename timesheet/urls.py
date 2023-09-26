1e87e971d8a706b3befae53738f66b79b173eceb
Robert Gray <rgray@ttsa.net>
2023-09-26 14:07:53 -0700

    last commit on main before adding scheduler application

--- timesheet/urls.py
+++ timesheet/urls.py
@@ -1,15 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic.base import TemplateView
+from django.urls import path
+from . import views
+# from .views import VendorListView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('employee.urls')),
-    path('timesheetentries/', include('timesheets.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('manage/', include('tsmanagement.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('message/', include('mailer.urls')),
-    path('purchasing/', include('purchasing.urls')),
-    path('demo', TemplateView.as_view(template_name="bootstrap_base.html"), name='demo'),
-    path('popovers', TemplateView.as_view(template_name="bootstrap_popovers.html"), name="popovers"),
-]
+    path('company', views.company, name='company'),
+    # path('manufacturer', views.company, name='company'),
+    path('vendors', views.FilteredCompanyListView.as_view()),
+    path('manufacturers', views.FilteredCompanyListView.as_view()),
+    path('api/company', views.CompanyApiView, name='api_company')
+    ]
