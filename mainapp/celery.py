fbce4c4b1a6cb904d1a60a48390600850f8a2a21
Robert Gray <rgray@ttsa.net>
2023-10-30 21:22:20 -0700

    changed celery.py file to point to mainapp

--- mainapp/celery.py
+++ mainapp/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timesheet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainapp.settings')
 
-app = Celery('timesheet')
+app = Celery('mainapp')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
