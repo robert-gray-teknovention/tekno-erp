b5fbdbbf6d8db8f5a0cf07295512581cc51bee48
Robert Gray <rgray@teknovention.com>
2025-12-08 20:45:11 -0800

    Finished cleaning up workorder list, details, and workentry list and details.  Added workorders to side menu. Uploading to server.

--- workorders/tables.py
+++ workorders/tables.py
@@ -16,7 +16,6 @@ class WorkOrderTable(tables.Table):
         return ", ".join([str(u) for u in record.assigned_to.all()])
 
     actions = tables.TemplateColumn(
-        "<a href='/workorders/{{record.id}}/edit/' class='btn btn-sm btn-outline-primary me-1'>Edit</a>"
         "<a href='/workorders/{{record.id}}/delete/' class='btn btn-sm btn-outline-danger'>Delete</a>",
         verbose_name="Actions",
         orderable=False,
@@ -24,21 +23,20 @@ class WorkOrderTable(tables.Table):
 
     class Meta:
         model = WorkOrder
-        sequence = ("create_date", "project", "equipment", "status", "assigned_to", "request", "actions")
-        exclude = ("notes", "is_active", "creator")
+        sequence = ("id", "create_date", "project", "equipment", "status", "assigned_to", "request", "actions")
+        exclude = ("notes", "is_active", "creator", "start_date", "end_date", "scheduled_at")
 
 
 class WorkEntryTable(tables.Table):
-    def render_work_order(self, value, record):
+    def render_notes(self, value, record):
         if value:
-            return format_html("<a href='{}'>WO #{}</a>", f"/workorders/{value.id}/edit/", value.id)
+            return format_html("<a href='/workorders/workentries/{}/edit/?next=/workorders/{}/'>​{}</a>", record.id, record.work_order.id, value)
         return ""
 
     def render_user(self, value, record):
         return str(value)
 
     actions = tables.TemplateColumn(
-        "<a href='/workorders/workentries/{{record.id}}/edit/?next=/workorders/{{record.work_order.id}}/' class='btn btn-sm btn-outline-primary me-1'>Edit</a>"
         "<a href='/workorders/workentries/{{record.id}}/delete/?next=/workorders/{{record.work_order.id}}/' class='btn btn-sm btn-outline-danger'>Delete</a>",
         verbose_name="Actions",
         orderable=False,
@@ -46,5 +44,5 @@ class WorkEntryTable(tables.Table):
 
     class Meta:
         model = WorkEntry
-        sequence = ("user", "date_time_in", "duration", "project", "work_order", "actions")
-        exclude = ("docs", "approver_approved", "period", "hourly_rate")
+        sequence = ("id", "date_time_in", "date_time_out", "duration", "project", "notes", "actions")
+        exclude = ("docs", "approver_approved", "period", "hourly_rate", "timesheetentry_ptr", "is_approved", "user", "date_time_entry ", "work_order", "date_time_entry")
